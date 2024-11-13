25659b7c9960c5218f33c29f9e97f191a8c63a3b
mhaloz <146088043+mhaloz@users.noreply.github.com>
2024-11-13 15:11:18 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,5 +8,5 @@ module "ecs_cluster" {
   region         = "us-east-1" # Cambia esto a tu región preferida
   cluster_name   = "my-ecs-cluster"
   subnets        = ["subnet-05fc7e67f1f1464cc", "subnet-0cb89298095599d21"] # Cambia esto a tus subnets
-  security_group = "sg-058da4341e718b395" # Cambia esto a tu security group
+  security_group = "sg-00edfee3e65eb63da" # Cambia esto a tu security group
 }
