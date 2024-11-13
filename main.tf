cce7b577e0410f40c377706157468721e7a5442f
mhaloz <146088043+mhaloz@users.noreply.github.com>
2024-11-13 17:07:40 +0000

    region

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 # main.tf
 provider "aws" {
-  region = "us-west-2" # Cambia esto a tu región preferida
+  region = "us-east-1" # Cambia esto a tu región preferida
 }
 
 module "ecs_cluster" {
   source         = "./ecs_module"
-  region         = "us-west-2" # Cambia esto a tu región preferida
+  region         = "us-east-1" # Cambia esto a tu región preferida
   cluster_name   = "my-ecs-cluster"
   subnets        = ["subnet-05fc7e67f1f1464cc", "subnet-0cb89298095599d21"] # Cambia esto a tus subnets
   security_group = "sg-058da4341e718b395" # Cambia esto a tu security group
